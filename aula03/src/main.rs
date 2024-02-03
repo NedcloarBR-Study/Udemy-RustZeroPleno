94ba68a2d65ab29e37ce29c4640a6660d97b95d1
NedcloarBR <nedcloar1@hotmail.com>
2024-02-03 20:36:57 -0300

    feat(aula03): if else
    Check if a number is greater than other with if and else then print the result

---
+++ aula03/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+  let number1: i32 = 24;
+  let number2: i32 = 42;
+  if number1 > number2 {
+    println!("{} > {}", number1, number2)
+  } else {
+    println!("{} <= {}", number1, number2)
+  }
+}
