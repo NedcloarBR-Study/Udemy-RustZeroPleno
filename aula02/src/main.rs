9f751d48e8a309c69be8b685060cfacfda792690
NedcloarBR <nedcloar1@hotmail.com>
2024-02-03 20:33:47 -0300

    feat(aula02): types of variables

---
+++ aula02/src/main.rs
@@ -0,0 +1,36 @@
+fn main() {
+  let x: i32 = 22;
+  let f: f64 = 6.7;
+  let b: bool = true;
+}
+
+/*
+*  Signed integers | positive and negative numbers
+** : i8   (8 bits)
+** : i16  (16 bits)
+** : i32  (32 bits) //! default
+** : i64  (64 bits)
+** : i126 (128 bits)
+** etc...
+*/
+
+/*
+*  Unsigned integers | only positive numbers
+** : u8   (8 bits)
+** : u16  (16 bits)
+** : u32  (32 bits)
+** : u64  (64 bits)
+** : u126 (128 bits)
+** etc...
+*/
+
+/*
+*  Float numbers
+** : f32
+** : f64 //! default
+*/
+
+/*
+*  Booleans | true or false
+** : bool
+*/
