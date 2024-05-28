a00bf70f0e3f28b4d31647b36f1a6efaca2a4095
NedcloarBR <nedcloar1@hotmail.com>
2024-05-27 21:48:37 -0300

    feat: add aula01

---
+++ modulos/02/aulas/01/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+  let tupla = (12, "valores", 3.14, (1, 2, 3));
+  let (a, b, c, d) = tupla;
+  println!("{}", (tupla.3).2);
+  println!("O valor de a é: {}", a);
+  println!("O valor de b é: {}", b);
+  println!("O valor de c é: {}", c);
+  println!("O valor de d é: {:?}", d);
+}
