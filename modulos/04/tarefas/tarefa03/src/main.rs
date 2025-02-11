50ba0a83ed13917e9d3baab7598f86e20d6b63f3
NedcloarBR <nedcloar1@hotmail.com>
2025-02-11 15:55:41 -0300

    feat: update tarefa03

--- modulos/04/tarefas/tarefa03/src/main.rs
+++ modulos/04/tarefas/tarefa03/src/main.rs
@@ -1,6 +1,29 @@
+use std::{
+  fs::File,
+  io::{BufRead, BufReader, Error},
+  process,
+};
+
 use tarefa03::exec;
 
+fn ler_numeros(nome_arquivo: &str) -> Result<Vec<i32>, Error> {
+  let arquivo = File::open(nome_arquivo)?;
+  let leitor = BufReader::new(arquivo);
+  let mut numeros = vec![];
+  for linha in leitor.lines() {
+    let linha = linha?;
+    if let Ok(numero) = linha.trim().parse::<i32>() {
+      numeros.push(numero);
+    }
+  }
+
+  return Ok(numeros);
+}
+
 fn main() {
-  let numeros = vec![1, 1, 2, 3, 4, 4, 4, 5, 6, 0];
+  let numeros = ler_numeros("./src/dados.txt").unwrap_or_else(|err| {
+    eprintln!("Erro ao ler o arquivo: {}", err);
+    process::exit(1);
+  });
   exec(numeros);
 }
